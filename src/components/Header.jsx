ab5b676c98b5da86c714fcd9f1b53cd11536ecb5
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-04-03 14:20:25 +0200

    feat: add element to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,22 @@ function Header () {
     return (
         <header>
             
-            //logo
+            {/* logo */}
+            <img src='../../public/dc-logo.png' alt='logo' />
+
+            {/* list */}
+            <ul>
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
 
         </header>
     );
