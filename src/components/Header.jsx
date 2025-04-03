3f40eed75c6d392095cf896040913f8e7ae2c8e0
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-04-03 15:11:44 +0200

    feat: add link to li

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,16 +10,16 @@ function Header () {
 
             {/* list */}
             <ul>
-                <li>characters</li>
-                <li>comics</li>
-                <li>movies</li>
-                <li>tv</li>
-                <li>games</li>
-                <li>collectibles</li>
-                <li>videos</li>
-                <li>fans</li>
-                <li>news</li>
-                <li>shop</li>
+                <li><a href='/'>characters</a></li>
+                <li><a href='/'>comics</a></li>
+                <li><a href='/'>movies</a></li>
+                <li><a href='/'>tv</a></li>
+                <li><a href='/'>games</a></li>
+                <li><a href='/'>collectibles</a></li>
+                <li><a href='/'>videos</a></li>
+                <li><a href='/'>fans</a></li>
+                <li><a href='/'>news</a></li>
+                <li><a href='/'>shop</a></li>
             </ul>
 
         </header>
