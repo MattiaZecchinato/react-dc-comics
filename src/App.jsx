9c6e723480d851fb6df55fcc8ca7849be7de9fed
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-04-03 14:08:29 +0200

    feat: start project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// Header
+import Header from './components/Header';
+// Main
+import Main from './components/Main';
+// Footer
+import Footer from './components/Footer';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
