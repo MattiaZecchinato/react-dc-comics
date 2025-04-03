baed130c5737bc42010c0422bd785a37628f6d18
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-04-03 16:41:47 +0200

    feat: add style to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,22 +5,24 @@ function Header () {
     return (
         <header>
             
-            {/* logo */}
-            <img src={dcLogo} alt='logo' />
+            <div className='header-box'>
+                {/* logo */}
+                <img src={dcLogo} alt='logo' />
 
-            {/* list */}
-            <ul>
-                <li><a href='/'>characters</a></li>
-                <li><a href='/'>comics</a></li>
-                <li><a href='/'>movies</a></li>
-                <li><a href='/'>tv</a></li>
-                <li><a href='/'>games</a></li>
-                <li><a href='/'>collectibles</a></li>
-                <li><a href='/'>videos</a></li>
-                <li><a href='/'>fans</a></li>
-                <li><a href='/'>news</a></li>
-                <li><a href='/'>shop</a></li>
-            </ul>
+                {/* list */}
+                <ul>
+                    <li><a href='/'>characters</a></li>
+                    <li><a href='/'>comics</a></li>
+                    <li><a href='/'>movies</a></li>
+                    <li><a href='/'>tv</a></li>
+                    <li><a href='/'>games</a></li>
+                    <li><a href='/'>collectibles</a></li>
+                    <li><a href='/'>videos</a></li>
+                    <li><a href='/'>fans</a></li>
+                    <li><a href='/'>news</a></li>
+                    <li><a href='/'>shop</a></li>
+                </ul>
+            </div>
 
         </header>
     );
