f3d1f58411906aff6793830c829a694fdd8db9ce
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-04-07 16:16:34 +0200

    feat: add jumbotron

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,20 +5,32 @@ import BlueList from './BlueList';
 
 import comicsList from '../data/comics.js';
 
+import jumbotron from '../assets/jumbotron.jpg'
+
 function Main () {
 
     return (
         <main>
-            
-            <div className='content-box'>
-                {/* paragraph */}
-                <ul className='comics-box'>
-                    {comicsList.map((elem, i) => 
-                        (<li key={i}>
-                            <ComicCard title={elem.title} series={elem.series} img={elem.thumb} />
-                        </li>)
-                    )}
-                </ul>
+
+            <div className='container-jumbotron'>
+                <img src={jumbotron} alt="jumbotron" />
+            </div>
+
+            <div className='main-content'>
+                <div className='current-series'>current series</div>
+                
+                <div className='content-box'>
+                    {/* paragraph */}
+                    <ul className='comics-box'>
+                        {comicsList.map((elem, i) => 
+                            (<li key={i}>
+                                <ComicCard title={elem.title} series={elem.series} img={elem.thumb} />
+                            </li>)
+                        )}
+                    </ul>
+                </div>
+
+                <a href='/' className='load-more-btn'>load more</a>
             </div>
 
             {/* blue list */}
